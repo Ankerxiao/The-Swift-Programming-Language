5d6a2b38da888c7bd2858fbc3d77a3c26c953f1b
晓锟 <anker.huang@scysun.com>
2018-10-19 20:44:25 +0800

    类和结构体

---
+++ Class_Struct.playground/Contents.swift
@@ -0,0 +1,13 @@
+struct Resolution {
+    var width = 0
+    var height = 1
+}
+
+class VideoMode {
+    var resolution = Resolution()
+    var interlaced = false
+    var frameRate = 0.0
+    var name: String?
+}
+let someResolution = Resolution()
+let someVideoMode = VideoMode()
