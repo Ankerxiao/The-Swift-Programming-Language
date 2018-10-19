a26fba9ba5f55503a6c8c28d5c2d6b57c283a45d
晓锟 <anker.huang@scysun.com>
2018-10-19 22:25:48 +0800

    添加新功能

--- Class_Struct.playground/Contents.swift
+++ Class_Struct.playground/Contents.swift
@@ -11,3 +11,45 @@ class VideoMode {
 }
 let someResolution = Resolution()
 let someVideoMode = VideoMode()
+print("The width of someResolution is \(someResolution.width)")
+print("The width of someVideoMode is \(someVideoMode.resolution.width)")
+someVideoMode.resolution.width = 1280
+print("The width of someVideoMode is now \(someVideoMode.resolution.width)")
+let vga = Resolution(width:640, height: 480)
+print("vga width: \(vga.width), vga height: \(vga.height)")
+let hd = Resolution(width: 1920, height: 1080)
+var cinema = hd
+cinema.width = 2048
+print("cinema is now  \(cinema.width) pixels wide")
+print("hd is still \(hd.width) pixels wide")
+
+enum CompassPoint {
+    case North, South, East, West
+}
+var currentDirection = CompassPoint.West
+let rememberedDirection = currentDirection
+currentDirection = .East
+if rememberedDirection == .West {
+    print("The remembered direction is still .West")
+}
+
+let tenEighty = VideoMode()
+tenEighty.resolution = hd
+tenEighty.interlaced = true
+tenEighty.name = "1080i"
+tenEighty.frameRate = 25.0
+print("The frameRate property of tenEighty is now \(tenEighty.frameRate)")
+let alsoTenEighty = tenEighty
+alsoTenEighty.frameRate = 30.0
+print("The frameRate property of tenEighty is now \(tenEighty.frameRate)")
+
+/*
+ 请注意，“等价于”（用三个等号表示，===）与“等于”（用两个等号表示，==）的不同：
+ 
+ “等价于”表示两个类类型（class type）的常量或者变量引用同一个类实例。
+ “等于”表示两个实例的值“相等”或“相同”，判定时要遵照设计者定义的评判标准，因此相对于“相等”来说，这是一种更加合适的叫法。
+ */
+if tenEighty === alsoTenEighty {
+    print("tenEighty and alsoTenEighty refer to the same Resolution instance.")
+}
+
