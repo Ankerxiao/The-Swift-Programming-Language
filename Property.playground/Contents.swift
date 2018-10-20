84f2fc5462b4ffe71b0b8709c6b9c6ed1cd90522
晓锟 <anker.huang@scysun.com>
2018-10-20 09:10:40 +0800

    Property

---
+++ Property.playground/Contents.swift
@@ -0,0 +1,7 @@
+struct FixedLengthRange {
+    var firstValue: Int
+    let length: Int
+}
+var rangeOfThreeItems = FixedLengthRange(firstValue: 0, length: 3)
+// 该区间表示整数0，1，2
+rangeOfThreeItems.firstValue = 6
