b990e09ac6871c6374f080c7d2ac16dc8b7adfa5
晓锟 <anker.huang@scysun.com>
2018-10-21 19:35:25 +0800

    Method

---
+++ Method.playground/Contents.swift
@@ -0,0 +1,118 @@
+class Counter {
+    var count = 0
+    func increment() {
+        count += 1
+        self.count += 1
+    }
+    func increment(by amount: Int) {
+        count += amount
+    }
+    func reset() {
+        count = 0
+    }
+}
+let counter = Counter()
+print(counter.count)
+counter.increment()
+print(counter.count)
+counter.increment(by: 5)
+print(counter.count)
+counter.reset()
+print(counter.count)
+
+struct Point {
+    var x = 0.0, y = 0.0
+    func isToTheRightOfX(x: Double) -> Bool {
+        return self.x > x
+    }
+}
+let somePoint = Point(x: 4.0, y: 5.0)
+if somePoint.isToTheRightOfX(x: 1.0) {
+    print("This point is to the right of the line where x == 1.0")
+}
+
+struct PointMutable {
+    var x = 0.0, y = 0.0
+    mutating func moveByX(x deltaX: Double, y deltaY: Double) {
+        x += deltaX
+        y += deltaY
+    }
+}
+var somePoint2 = PointMutable(x: 1.0, y: 1.0)
+somePoint2.moveByX(x: 2.0, y: 3.0)
+print("The point is now at (\(somePoint2.x), \(somePoint2.y))")
+// 打印 "The point is now at (3.0, 4.0)"
+
+struct Point3 {
+    var x = 0.0, y = 0.0
+    mutating func moveBy(x deltaX: Double, y deltaY: Double) {
+        self = Point3(x: x + deltaX, y: y + deltaY)
+    }
+}
+
+enum TriStateSwitch {
+    case Off, Low, High
+    mutating func next() {
+        switch self {
+        case .Off:
+            self = .Low
+        case .Low:
+            self = .High
+        case .High:
+            self = .Off
+        }
+    }
+}
+var ovenLight = TriStateSwitch.Low
+ovenLight.next()
+// ovenLight 现在等于 .High
+ovenLight.next()
+// ovenLight 现在等于 .Off
+
+
+
+struct LevelTracker {
+    static var highestUnlockedLevel = 1
+    var currentLevel = 1
+    
+    static func unlock(_ level: Int) {
+        if level > highestUnlockedLevel { highestUnlockedLevel = level }
+    }
+    
+    static func isUnlocked(_ level: Int) -> Bool {
+        return level <= highestUnlockedLevel
+    }
+    
+    @discardableResult
+    mutating func advance(to level: Int) -> Bool {
+        if LevelTracker.isUnlocked(level) {
+            currentLevel = level
+            return true
+        } else {
+            return false
+        }
+    }
+}
+class Player {
+    var tracker = LevelTracker()
+    let playerName: String
+    func complete(level: Int) {
+        LevelTracker.unlock(level + 1)
+        tracker.advance(to: level + 1)
+    }
+    init(name: String) {
+        playerName = name
+    }
+}
+var player = Player(name: "Argyrios")
+player.complete(level: 1)
+print("highest unlocked level is now \(LevelTracker.highestUnlockedLevel)")
+// 打印 "highest unlocked level is now 2"
+
+player = Player(name: "Beto")
+if player.tracker.advance(to: 6) {
+    print("player is now on level 6")
+} else {
+    print("level 6 has not yet been unlocked")
+}
+// 打印 "level 6 has not yet been unlocked"
